d439ebe1167ae9526aedc24fcd02dbd7bb325554
Norik <noriksaroyan@gmail.com>
2024-05-29 02:18:18 +0300

    everything is working. Done

--- UI/diagram/transmissionchart.cpp
+++ UI/diagram/transmissionchart.cpp
@@ -1,15 +1,36 @@
 #include <QVBoxLayout>
-
 #include "transmissionchart.h"
+#include <QMainWindow>
 #include <QtCharts/QChart>
 #include <QtCharts/QPieSeries>
+#include "QPushButton"
+#include "../cars/catalog/carcatalogpage.h"
+
+class CarCatalogPage;
 
 TransmissionChart::TransmissionChart(QWidget *parent)
-    : QWidget(parent) {
+    : QWidget(parent), chartView(new QChartView(this)) {
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+
     orderService = new OrderService();
     carService = new CarService();
-    createChart();
 
+    // Create the button and add it to the layout
+    goToCarCatalogButton = new QPushButton("Go to Car Catalog", this);
+    goToCarCatalogButton->setFixedSize(200, 50); // Adjust the size as needed
+
+    connect(goToCarCatalogButton, &QPushButton::clicked, this, &TransmissionChart::goToCarCatalog);
+
+    createChart();
+    chartView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    chartView->setMinimumSize(800, 600); // Adjust the minimum size as needed
+
+    // Add the HeaderWidget, chartView, and back button to the layout
+    mainLayout->addWidget(chartView);
+    mainLayout->addStretch(1);
+    mainLayout->addWidget(goToCarCatalogButton);
+
+    setLayout(mainLayout);
 }
 
 void TransmissionChart::createChart() {
@@ -39,10 +60,13 @@ void TransmissionChart::createChart() {
     chart->legend()->setVisible(true);
     chart->legend()->setAlignment(Qt::AlignRight);
 
-    chartView = new QChartView(chart);
+    chartView->setChart(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
+}
 
-    auto *layout = new QVBoxLayout(this);
-    layout->addWidget(chartView);
-    setLayout(layout);
+void TransmissionChart::goToCarCatalog() {
+    CarCatalogPage *carCatalogPage = new CarCatalogPage(new QMainWindow);
+    QMainWindow *parent = dynamic_cast<QMainWindow *>(this->parentWidget());
+    parent->setCentralWidget(carCatalogPage);
+    this->parentWidget()->show();
 }
--- UI/diagram/transmissionchart.h
+++ UI/diagram/transmissionchart.h
@@ -8,6 +8,7 @@
 
 #include "../../../Services/order/OrderService.h"
 #include "../../../Services/car/CarService.h"
+class QPushButton;
 
 class TransmissionChart : public QWidget {
     Q_OBJECT
@@ -15,9 +16,13 @@ class TransmissionChart : public QWidget {
 public:
     explicit TransmissionChart(QWidget *parent = nullptr);
 
+public slots:
+    void goToCarCatalog();
+
 private:
     void createChart();
 
+    QPushButton *goToCarCatalogButton;
     OrderService *orderService;
     CarService *carService;
     QChartView *chartView;
